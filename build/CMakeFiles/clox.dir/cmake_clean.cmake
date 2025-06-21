2a1b15d16a6b2d4807b99268fc7e89cc79c7928e
z4rathustr4 <ringzer0dev@gmail.com>
2025-06-21 12:26:14 +0100

    test

---
+++ build/CMakeFiles/clox.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clox.dir/src/chunk.c.o"
+  "CMakeFiles/clox.dir/src/chunk.c.o.d"
+  "CMakeFiles/clox.dir/src/compiler.c.o"
+  "CMakeFiles/clox.dir/src/compiler.c.o.d"
+  "CMakeFiles/clox.dir/src/debug.c.o"
+  "CMakeFiles/clox.dir/src/debug.c.o.d"
+  "CMakeFiles/clox.dir/src/lexer.c.o"
+  "CMakeFiles/clox.dir/src/lexer.c.o.d"
+  "CMakeFiles/clox.dir/src/main.c.o"
+  "CMakeFiles/clox.dir/src/main.c.o.d"
+  "CMakeFiles/clox.dir/src/memory.c.o"
+  "CMakeFiles/clox.dir/src/memory.c.o.d"
+  "CMakeFiles/clox.dir/src/value.c.o"
+  "CMakeFiles/clox.dir/src/value.c.o.d"
+  "CMakeFiles/clox.dir/src/vm.c.o"
+  "CMakeFiles/clox.dir/src/vm.c.o.d"
+  "clox"
+  "clox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
